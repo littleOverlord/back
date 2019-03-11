eb470f12bbdc5c3a90acc4fccd697f13a41a2e78
tdd <tdd_style@163.com>
2019-03-11 19:51:30 +0800

    add https support

--- htdocs/ni/server/index.js
+++ htdocs/ni/server/index.js
@@ -9,6 +9,7 @@ const fs = require("fs");
 const http = require("http");
 const https = require("https");
 //ni
+const NI = require("../index");
 const Util = require("../util");
 const router = require("../router");
 const log = require("../log");
@@ -21,28 +22,33 @@ const log = require("../log");
  */
 class Start{
     static http(_cfg){
-        const server = http.createServer((req, res) => {
-            Util.tryCatch(()=>{
-                router.response(req, res);
-            },(error) => {
-                res.writeHead(500,{"content-type":"text/plain"});
-                res.write(log.clientInfo(500,error.message));
-                res.end();
-                log.add(error,"error");
-            });
-        });
-        server.on('clientError', (err, socket) => {
-            socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-        });
-        server.listen(_cfg.port);
-        console.log(`http server start at port ${_cfg.port}`)
-          
+        create(https,{},_cfg.port);  
     }
     static https(_cfg){
-
+        const options = {
+            key: fs.readFileSync(NI.mergeAbs(_cfg.key)),
+            cert: fs.readFileSync(NI.mergeAbs(_cfg.crt))
+          };
+        create(https,options,_cfg.port);
     }
 }
-
+const create = (mod,options,port) => {
+    const server = mod.createServer(options,(req, res) => {
+        Util.tryCatch(()=>{
+            router.response(req, res);
+        },(error) => {
+            res.writeHead(500,{"content-type":"text/plain"});
+            res.write(log.clientInfo(500,error.message));
+            res.end();
+            log.add(error,"error");
+        });
+    });
+    server.on('clientError', (err, socket) => {
+        socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+    });
+    server.listen(port);
+    
+}
 /***** Module exports *****/
 /**
  * @description 启动http(s)服务器
@@ -51,6 +57,7 @@ exports.serverStart  = (cfg) => {
     for(let k in cfg.server){
         Start[k] && Start[k](cfg.server[k]);
     }
+    console.log(`http server start!`)
 }
 
 /***** local running ******/
