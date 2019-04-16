1633f93f49ea5f3f206302c71a39189e495491bb
tdd <tdd_style@163.com>
2019-04-16 15:52:25 +0800

    fix  wx login

--- htdocs/app/wx/index.js
+++ htdocs/app/wx/index.js
@@ -49,7 +49,7 @@ exports.login = (rq,res,search) => {
         if(data.errcode){
             return Util.httpResponse(res,500,log.clientInfo(500,data.errmsg));
         }
-        pc = new WXBizDataCrypt(config.appId, data.sessionKey);
+        pc = new WXBizDataCrypt(config.appId, data.session_key);
         data = pc.decryptData(encrypted , iv);
         console.log(data);
         db.findOne("user",{uid:data.unionid},(err,result)=>{
